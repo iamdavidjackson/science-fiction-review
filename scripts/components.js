6911eeb64a1c5fc6a94b4bdc510c25456ca0a8ca
David Jackson <davidjackson123@gmail.com>
2023-02-24 09:18:59 -0500

    add: initial carousel logic

--- scripts/components.js
+++ scripts/components.js
@@ -1,1 +1,42 @@
-console.log('here');
+$(document).ready(function () {
+  
+  const carousels = [];
+  $('.carousel').each(function () {
+    carousels.push(new Carousel(this));
+  });
+});
+
+function Carousel(element) {
+  const $carousel = $(element);
+
+  function setWidths() {
+    const $carouselElements = $carousel.find('.slide');
+    const slidesLength = $carouselElements.length;
+    const slideWidth = $($carouselElements[0]).width() + 5;
+    $carousel.width(slidesLength * slideWidth);
+  }
+
+  function onResize() {
+    setWidths();
+  }
+
+  function addEventListeners() {
+    window.addEventListener('resize', onResize);
+  }
+
+  function removeEventListeners() {
+    window.removeEventListener('resize', onResize);
+  }
+
+  function init() {
+    setWidths();
+    addEventListeners();
+  }
+
+  function destroy() {
+    removeEventListeners();
+  }
+
+  init();
+  
+}
